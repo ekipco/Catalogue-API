9b2524632f71b7bb3d142ddb96013cbc6003dd47
cemil.sevim@angular.com.tr <cemil.sevim@hotmail.com>
2021-12-10 19:21:48 +0300

    Updated db.json. Added public images.

--- app.ts
+++ app.ts
@@ -5,9 +5,11 @@ const app = express();
 const port = 3000; // default port to listen
 
 app.use(express.json({ strict: false }));
+app.use(express.static('public'));
 
 // define a route handler for the default home page
 app.use('/', apiRouter);
+
 
 // start the Express server
 app.listen(port, () => {
