694f1c422447004828024e032249089c49044135
cemil.sevim@angular.com.tr <cemil.sevim@hotmail.com>
2021-12-20 13:21:26 +0300

    added cors

--- app.ts
+++ app.ts
@@ -1,15 +1,16 @@
 import express from 'express';
 import apiRouter from './routes';
+import cors from "cors";
 
 const app = express();
 const port = 3000; // default port to listen
 
 app.use(express.json({ strict: false }));
 app.use(express.static('public'));
+app.use(cors());
 
 // define a route handler for the default home page
 app.use('/', apiRouter);
-
 
 // start the Express server
 app.listen(port, () => {
